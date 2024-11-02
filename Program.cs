73c724b35a70a50a2d90b1200fd2e3d88d051b63
Sahayanathan George Dilen <139237758+dilen1999@users.noreply.github.com>
2024-11-02 23:20:29 +0530

    data types

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
 
 namespace ConsoleApplciation2
 {
@@ -7,7 +7,21 @@ namespace ConsoleApplciation2
     {
         static void Main(string[] args)
         {
+            string name;
+            char Section;
+            int number;
+            double percentage;
 
+            name = "Dilen";
+            Section = 'A';
+            number = 13;
+            percentage = 14.67;
+
+            Console.WriteLine(name);
+            Console.WriteLine(Section);
+            Console.WriteLine(number);
+            Console.WriteLine(percentage);
+            Console.ReadLine();
         }
     }
 }
